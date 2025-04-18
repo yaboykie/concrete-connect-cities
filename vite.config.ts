e04e6dc94e7fdf34176a358cdd11e1047cf2baa4
yaboykie <kierontaylor1995@gmail.com>
2025-04-18 18:41:05 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-
 import { defineConfig } from 'vite'
-import { react } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // ✅ FIXED default import
 import * as path from 'path'
 import { componentTagger } from 'lovable-tagger'
 
